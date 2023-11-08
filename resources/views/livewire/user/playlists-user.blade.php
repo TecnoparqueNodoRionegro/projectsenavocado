e0be8d0d6b5c14a347fdcf14ea2e3cac1a19cf2f
Harold marin <haroldmarin2011@gmail.com>
2023-11-08 11:41:45 -0500

    Add img

--- resources/views/livewire/user/playlists-user.blade.php
+++ resources/views/livewire/user/playlists-user.blade.php
@@ -26,8 +26,6 @@
                             </div>
                             
                         </div>
-                        <iframe width="560" height="315" src="{{ $playlist->url_playlist }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                        </iframe >
                     @endif
                 @endforeach
             </div>
